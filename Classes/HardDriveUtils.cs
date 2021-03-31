1b17e66a45271dfc5db3b34458bf257ac2b276c5
Jelmur <Jelmervdermeulen@gmail.com>
2021-03-31 19:36:52 +0200

    Added a HardDriveUtils class and delete button on the PhotoPage now deletes the imagefiles from the local directory.

---
+++ Classes/HardDriveUtils.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FotoShop.Classes
+{
+    public class HardDriveUtils
+    {
+        public string GetFilePath(IFormFile imageFile)
+        {
+            if(imageFile != null)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(imageFile.FileName);
+                string extension = Path.GetExtension(imageFile.FileName);
+                string file = fileName + extension;
+                return file;
+            }
+
+            return null;
+        }
+
+        public string GetDirPath(IFormFile imageFile, string categoryName)
+        {
+            string imagesDir = GetImgDir();
+            string filepath = GetFilePath(imageFile);
+            string dirPath = Path.Combine(imagesDir,categoryName, filepath);
+            return dirPath;
+        }
+
+        public string GetImgDir()
+        {
+            string imagesDir = Path.Combine(new DirectoryInfo(
+                Directory.GetCurrentDirectory()).FullName, "wwwroot", "Images", "ProductImages");
+            return imagesDir;
+        }
+    }
+}
--- Pages/UploadImage.cshtml.cs
+++ Pages/UploadImage.cshtml.cs
@@ -36,19 +36,15 @@ namespace FotoShop.Pages
                 NewPhoto.Price = NewPhoto.Price.Replace(',', '.');
                 if (float.TryParse(NewPhoto.Price, out float price ))
                 {
-                    string imagesDir = Path.Combine(new DirectoryInfo(
-                        Directory.GetCurrentDirectory()).FullName, "wwwroot", "Images", "ProductImages");
-                    string fileName = Path.GetFileNameWithoutExtension(ImageFile.FileName);
-                    string extension = Path.GetExtension(ImageFile.FileName); 
-                    string file = fileName + extension;
-                    string path = Path.Combine(imagesDir,NewPhoto.Category_name, file);
-                    NewPhoto.Photo_path = string.Format("{0}/{1}{2}", NewPhoto.Category_name, fileName, extension);
+                    string file = new HardDriveUtils().GetFilePath(ImageFile);
+                    string dirPath = new HardDriveUtils().GetDirPath(ImageFile, NewPhoto.Category_name);
+                    NewPhoto.Photo_path = string.Format("{0}/{1}", NewPhoto.Category_name, file);
                     using PhotoRepository repo = new PhotoRepository(DbUtils.GetDbConnection());
                     repo.Add(NewPhoto);
-                    using (var fileStream = new FileStream(path, FileMode.Create))
-                    {
-                        await ImageFile.CopyToAsync(fileStream);
-                    }
+                    await using (var fileStream = new FileStream(dirPath, FileMode.Create))
+                     {
+                         await ImageFile.CopyToAsync(fileStream);
+                     }
                     ModelState.Clear();
                     return Redirect("UploadImage"); 
                 }
