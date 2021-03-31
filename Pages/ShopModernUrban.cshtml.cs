54a16457588e67766fb96a1918529590a3aea89f
Jelmur <Jelmervdermeulen@gmail.com>
2021-03-31 13:58:32 +0200

    Automatically loads images on Modern Urban page

--- Pages/ShopModernUrban.cshtml.cs
+++ Pages/ShopModernUrban.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FotoShop.Classes;
 using FotoShop.Classes.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FotoShop.Pages
@@ -9,17 +11,16 @@ namespace FotoShop.Pages
     {
         public void OnGet()
         {
-
+           
         }
-
-        public IEnumerable<Photo> AllPhotos
+        
+        public IList<Photo> AllPhotos
         {
             get
             {
                 using PhotoRepository repo = new PhotoRepository(DbUtils.GetDbConnection());
-                return repo.GetList("ModernUrban");
+                return repo.GetList("Modern Urban");
             }
         }
-
     }
 }
