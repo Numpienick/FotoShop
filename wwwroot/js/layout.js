15a17c0531380520481e25d649e9cc307db7d757
Nick Numan <nicknuman@gmail.com>
2021-03-31 16:14:52 +0200

    Added Ajax page to handle global ajax request such as the register modal. Fixed some margins in the modals. Removed index.js and ContactJS.js

--- wwwroot/js/layout.js
+++ wwwroot/js/layout.js
@@ -13,6 +13,42 @@
 		StyleDropdown();
 	});
 
+	var placeholderElement = $('#modal-placeholder');
+	$(document).on('click', '[data-toggle="ajax-modal"]', function () {
+		$(".modal-backdrop").remove();
+		var url = $(this).data('url');
+		$.get(url).done(function (data) {
+			placeholderElement.html(data);
+			placeholderElement.find('.modal').modal('show');
+		});
+	});
+
+	placeholderElement.on('click', '[data-save="modal"]', function (event) {
+		event.preventDefault();
+
+		var form = $(this).parents('.modal').find('form');
+		var actionUrl = form.attr('action');
+		var dataToSend = form.serialize();
+
+		$.post(actionUrl, dataToSend).done(function (data) {
+			var newBody = $('.modal-body', data);
+			placeholderElement.find('.modal-body').replaceWith(newBody);
+
+			var isValid = newBody.find('[name="IsValid"]').val() == 'True';
+			if (isValid) {
+				placeholderElement.find('.modal').modal('hide');
+				location.reload();
+			}
+		});
+	});
+
+	$("#logOut").click(function () {
+		var url = $(this).data('url');
+		$.get(url).done(function () {
+			location.reload();
+		});
+	});
+
 	function StyleDropdown() {
 		if (width >= 575) {
 			var dropdown = $(".dropdown-menu");
